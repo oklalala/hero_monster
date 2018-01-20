368680b7d5f0d74d7e253307e97a74bc690f4de4
Tony Lin <tonylin1208@gmail.com>
2018-01-20 22:37:17 +0800

    fix the wrong class to id in Hurt

--- js/index.js
+++ js/index.js
@@ -6,29 +6,53 @@ class BaseCharacter{
     this.ap = ap
     this.alive = true;
   }
-  attack(character, damage){
-    if(this.alive == false){
-      return;
-    }
-    character.getHert(damage);
 
-  }
-
-  getHert(damage){
+  getHurt(damage){
     this.hp -= damage;
     if (this.hp <= 0){
       this.die()
     }
+
+    var _this = this;
+    var i = 1;
+
+    _this.id = setInterval(function(){
+      if (i == 1){
+        _this.element.getElementsByClassName("effect-image")[0].style.display = "block";
+        _this.element.getElementsByClassName("hurt-text")[0].classList.add("attacked");
+        _this.element.getElementsByClassName("hurt-text")[0].textContent = damage;
+      }
+
+      _this.element.getElementsByClassName("effect-image")[0].src = "img/effect/blade/" +i+".png";
+      // _this.element.updateHtml(hpElement,hurtElement);
+      i++;
+
+      if (i > 8){
+        _this.element.getElementsByClassName("effect-image")[0].style.display = "none";
+        _this.element.getElementsByClassName("hurt-text")[0].classList.remove("attacked");
+        _this.element.getElementsByClassName("hurt-text")[0].textContent = "";
+        clearInterval(_this.id);
+      }
+    },50);
+
   }
 
   die(){
       this.alive = false;
   }
 
+  attack(character, damage){
+    if(this.alive == false){
+      return;
+    }
+    character.getHurt(damage);
+  }
+
   updateHtml(hpElement, hurtElement){
     hpElement.textContent = this.hp;
-    hurtElement.style.width = (1 - this.hp/this.maxHp)*100+"%";
+    hurtElement.style.width = (100 - this.hp/this.maxHp*100)+"%";
   }
+  
 }
 
 class Hero extends BaseCharacter{
@@ -37,8 +61,8 @@ class Hero extends BaseCharacter{
 
     this.element = document.getElementById("hero-image-block");
     this.hpElement = document.getElementById("hero-hp");
-    this.maxHpElement = document.getElementById("hero-max-hp");
     this.hurtElement = document.getElementById("hero-hp-hurt");
+    this.maxHpElement = document.getElementById("hero-max-hp");
 
     this.hpElement.textContent = this.hp;
     this.maxHpElement.textContent = this.maxHp;
@@ -126,8 +150,6 @@ function addSkillEvent(){
   skill.onclick = function(){
     heroAttack();
   }
-
-  
 }
 
 var hero = new Hero("Bernard", 120, 30);
